e6c7f1ceaf81b07f2113df449ba699555273f9fc
wrangelvid <wrangelvid@gmail.com>
2022-03-07 20:31:45 +0000

    added adaptive pure pursuit controller

--- scripts/parking_controller.py
+++ scripts/parking_controller.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
-
 import rospy
 import numpy as np
 
 from visual_servoing.msg import ConeLocation, ParkingError
 from ackermann_msgs.msg import AckermannDriveStamped
+
+from utilities.controllers import PurePursuit
+from utilities.Trajectory import LinearTrajectory
 
 class ParkingController():
     """
@@ -26,20 +28,72 @@ class ParkingController():
         self.relative_x = 0
         self.relative_y = 0
 
+        self.pursuit = PurePursuit(0.325)
+    
+    def v_function(self, v_desired, traj):
+        #adaptive velocity function
+        Lfw, lfw = 0, 0
+        v_desired = abs(v_desired)
+        if abs(v_desired) < 2:
+            Lfw = v_desired
+        elif v_desired >=2 and v_desired < 6:
+            Lfw = 1.5*v_desired
+        else:
+            Lfw = 12
+        return Lfw, lfw
+    
+    def drive(self, theta, speed, theta_dot = None,  acceleration = None):
+        """
+            Takes in steering and speed commands for the car.
+            :param theta: steering angle [rad]. right sided
+            :type theta: float
+            :param theta_dot: steering angular velocity [rad/s]
+            :type theta_dot: float
+            :param speed: speed in [m/s]
+            :type speed: float
+
+            :param speed: speed in [m/s]
+            :type speed: float
+        """
+        ack_msg = AckermannDriveStamped()
+        ack_msg.header.stamp = rospy.get_rostime()
+        ack_msg.header.frame_id = 'base_link'
+        ack_msg.drive.steering_angle = np.clip(theta, -0.34, 0.34)
+        ack_msg.drive.speed = speed
+        if not theta_dot is None:
+            ack_msg.drive.steering_angle_velocity = theta_dot
+        if not acceleration is None:
+            ack_msg.drive.acceleration = acceleration
+        return ack_msg
+    
+
     def relative_cone_callback(self, msg):
         self.relative_x = msg.x_pos
         self.relative_y = msg.y_pos
-        drive_cmd = AckermannDriveStamped()
 
+        dist = np.sqrt(self.relative_x**2 + self.relative_y**2) - self.parking_distance
+        heading = np.arctan2(self.relative_y, self.relative_x)
+
+        x_d = np.cos(heading)*dist
+        y_d = np.sin(heading)*dist
         #################################
 
-        # YOUR CODE HERE
-        # Use relative position and your control law to set drive_cmd
+        #generate trajectory 
+        traj_knots = np.array([0,0],
+                              [x_d, y_d],
+                              [x_d, y_d])
+        t_breaks = np.array([0,2,2.1])
+
+        trj_d = LinearTrajectory(t_breaks, traj_knots)
+
+        steer, speed = self.pursuit.adaptiveControl(trj_d, self.v_function)
+
+        drive_cmd = self.drive(steer, speed)
 
         #################################
 
         self.drive_pub.publish(drive_cmd)
-        self.error_publisher()
+        #self.error_publisher()
 
     def error_publisher(self):
         """
